3b20ece7c2962567b8e639ffa4c6ddb77dae7932
Fabian Jahr <fjahr@protonmail.com>
2019-12-07 15:06:47 +0100

    Fix initial test code issues

--- browser_plugin/src/inject/inject.js
+++ browser_plugin/src/inject/inject.js
@@ -1,13 +1,11 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-    clearInterval(readyStateCheckInterval);
+var readyStateCheckInterval = setInterval(function() {
+if (document.readyState === "complete") {
+  clearInterval(readyStateCheckInterval);
 
-    var search = location.search.split("&")[0].substring(3).split("+").join(" ");
-
-    console.log("Hello. This message was sent from scripts/inject.js");
-    console.log(search);
-
-	}
-	}, 10);
-});
+  console.log("Hello. This message was sent from scripts/inject.js");
+  if (window.location.host == "duckduckgo.com") {
+      var search = location.search.split("&")[0].substring(3).split("+").join(" ");
+      console.log(search);
+  }
+}
+}, 10);
