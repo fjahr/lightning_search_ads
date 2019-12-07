4bf953a31cff2fc4c16768a4c95153fa795c178f
Fabian Jahr <fjahr@protonmail.com>
2019-12-07 18:07:08 +0100

    implement some initial extension functionality

--- browser_plugin/src/inject/inject.js
+++ browser_plugin/src/inject/inject.js
@@ -1,11 +1,49 @@
 var readyStateCheckInterval = setInterval(function() {
 if (document.readyState === "complete") {
   clearInterval(readyStateCheckInterval);
+  var search;
 
-  console.log("Hello. This message was sent from scripts/inject.js");
   if (window.location.host == "duckduckgo.com") {
-      var search = location.search.split("&")[0].substring(3).split("+").join(" ");
-      console.log(search);
+      search = location.search.split("&")[0].substring(3).split("+").join(" ");
   }
+  console.log(search);
+
+  async function digestMessage(message) {
+    const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+    return hashHex;
+  }
+
+  const digestHex = digestMessage(search).then((hash) => {
+    console.log(hash);
+  });
+
+  // TODO: send hash to Ad Server, get back ads
+
+  var ads = [
+    {
+      "url": "https://gist.github.com/fjahr/44a0881a6da4012fd19b7f09df4408a7",
+      "text": "The first honey badger padding zoo in Berlin!",
+      "sats": 100
+    }
+  ]
+
+  if (ads.length > 0) {
+    // Insert ads into duckduckgo page
+    $("div#message").prepend("<div id='lnads'></div>")
+    $("div#lnads").append("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+    ads.forEach(function(ad) {
+      $("div#lnads").append(
+        "<p><a class='lnad' href='" + ad.url + "'>" + ad.text + " (" + ad.sats + " sats to view)</a></p>"
+      )
+    })
+    $("div#lnads").append("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+  }
+
+  // TODO: Add listeners to clicks on the ads
+  // TODO: Add redeem functionality
+
 }
 }, 10);
